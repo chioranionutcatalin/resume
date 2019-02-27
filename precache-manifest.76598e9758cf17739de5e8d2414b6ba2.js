25700c523c636eb1b0952c904725f46a25b9fd1a
chioranionutcatalin <chioranionutcatalin@gmail.com>
2019-02-27 23:00:20 +0200

    Updates

---
+++ precache-manifest.76598e9758cf17739de5e8d2414b6ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chioranionutcatalin.github.io/web-resume-2019/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d8e7ee1bb85835ce526a",
+    "url": "/chioranionutcatalin.github.io/web-resume-2019/static/js/runtime~main.d8e7ee1b.js"
+  },
+  {
+    "revision": "444096e75d7750c0133f",
+    "url": "/chioranionutcatalin.github.io/web-resume-2019/static/js/main.444096e7.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/chioranionutcatalin.github.io/web-resume-2019/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "444096e75d7750c0133f",
+    "url": "/chioranionutcatalin.github.io/web-resume-2019/static/css/main.58af14cd.chunk.css"
+  },
+  {
+    "revision": "18e814b00fc197df65599a04726f5f5b",
+    "url": "/chioranionutcatalin.github.io/web-resume-2019/index.html"
+  }
+];
